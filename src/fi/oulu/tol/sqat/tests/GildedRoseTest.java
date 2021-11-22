9c494f09522c5f98573380971665992f10fea6cd
juva <julius.valimaa@gmail.com>
2021-11-22 22:31:12 +0200

    Add main method test

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -29,4 +29,27 @@ public class GildedRoseTest {
 		//assert quality has decreased by one
 		assertEquals("Failed quality for Dexterity Vest", 19, quality);
 	}
+	
+	@Test
+	public void testMain() {
+		GildedRose inn = new GildedRose();
+		String[] args = null;
+		GildedRose.main(args);
+		
+		List<Item> items = inn.getItems();
+		int vestQuality = items.get(0).getQuality();
+		int brieQuality = items.get(1).getQuality();
+		int elixirQuality = items.get(2).getQuality();
+		int sulfurasQuality = items.get(3).getQuality();
+		int passQuality = items.get(4).getQuality();
+		int cakeQuality = items.get(5).getQuality();
+		
+		assertEquals("Failed quality for Dexterity Vest", 19, vestQuality);
+		assertEquals("Failed quality for Aged Brie", 1, brieQuality);
+		assertEquals("Failed quality for Elixir of the Mongoose", 6, elixirQuality);
+		assertEquals("Failed quality for Sulfuras, Hand of Ragnaros", 80, sulfurasQuality);
+		assertEquals("Failed quality for Backstage passes to a TAFKAL80ETC concert", 21, passQuality);
+		assertEquals("Failed quality for Conjured Mana Cake", 5, cakeQuality);
+	}
+	
 }
